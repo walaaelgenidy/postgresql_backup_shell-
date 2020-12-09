06b16893f892ed190ffc2d625094b55152e7c8c5
walaaelgenidy <45464802+walaaelgenidy@users.noreply.github.com>
2020-12-09 18:09:36 +0200

    Update Mysql.sh

--- Mysql.sh
+++ Mysql.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
- 
+
 export PATH=/bin:/usr/bin:/usr/local/bin
 TODAY=$(date +"%d%b%Y")
- 
+
 DB_BACKUP_PATH='/backup/sql'
-MYSQL_HOST='localhost'
-DBNAME = 'test'
+MYSQL_HOST='127.0.0.1'
+DB_NAME='test'
 MyUSER='testuser'
 MYSQL_PASSWD='F&9+7xZ`J%AXRgf'
 
-## Number of days to keep local backup copy
-BACKUP_RETAIN_DAYS=30   
- 
-mkdir -p ${DB_BACKUP_PATH}/${TODAY}
 
-for DB_NAME 
-	do
-		pg_dump -h ${MYSQL_HOST} -U ${MyUSER} -p ${MYSQL_PASSWD}  -d  ${DB_NAME} -f ${DB_BACKUP_PATH}/${TODAY}/${DB_NAME}.sql
-	done
+sudo mkdir  ${DB_BACKUP_PATH}/${TODAY}
+sudo -u postgres -i
+psql
+pg_dump -h ${MYSQL_HOST} -U ${MyUSER} -p ${MYSQL_PASSWD}  -d  ${DB_NAME} -f ${DB_BACKUP_PATH}/${TODAY}/${DB_NAME}.sql
 

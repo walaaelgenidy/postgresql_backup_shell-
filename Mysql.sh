902e2912c6309e9bb4b13a9ca3ab7ce15dea0974
walaaelgenidy <45464802+walaaelgenidy@users.noreply.github.com>
2020-12-09 17:20:32 +0200

    Update Mysql.sh

--- Mysql.sh
+++ Mysql.sh
@@ -5,27 +5,17 @@ TODAY=$(date +"%d%b%Y")
  
 DB_BACKUP_PATH='/backup/sql'
 MYSQL_HOST='localhost'
-
-MyUSER=''
-MYSQL_PASSWD=''
+DBNAME = 'test'
+MyUSER='testuser'
+MYSQL_PASSWD='F&9+7xZ`J%AXRgf'
 
 ## Number of days to keep local backup copy
 BACKUP_RETAIN_DAYS=30   
  
 mkdir -p ${DB_BACKUP_PATH}/${TODAY}
 
-for DB_NAME in ${DATABASE_NAMES}
+for DB_NAME 
 	do
-		pg_dump -h ${MYSQL_HOST} -u ${MyUSER} -p ${MYSQL_PASSWD}  --db ${DB_NAME} > ${DB_BACKUP_PATH}/${TODAY}/
+		pg_dump -h ${MYSQL_HOST} -U ${MyUSER} -p ${MYSQL_PASSWD}  -d  ${DB_NAME} -f ${DB_BACKUP_PATH}/${TODAY}/${DB_NAME}.sql
 	done
 
-######## Remove backups older than {BACKUP_RETAIN_DAYS} days  ########
- 
-DBDELDATE=$(date +"%d%b%Y" --date="${BACKUP_RETAIN_DAYS} days ago")
- 
-if [ ! -z ${DB_BACKUP_PATH} ]; then
-      cd ${DB_BACKUP_PATH}
-      if [ ! -z ${DBDELDATE} ] && [ -d ${DBDELDATE} ]; then
-            rm -rf ${DBDELDATE}
-      fi
-fi
